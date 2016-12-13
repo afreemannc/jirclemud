09cf7308b7bae49f7e89048900587f2fde9b515d
Allen Freeman <allen@designhammer.com>
2016-12-13 15:31:04 -0500

    Refactored room creation prompt

--- room.js
+++ room.js
@@ -93,29 +93,23 @@ room.prototype.loadExits = function(socket, roomId, callback, callbackArgs) {
 room.prototype.createRoom = function(socket) {
   var createRoomPrompt = prompt.new(socket, this.saveRoom);
 
-  var nameField = createRoomPrompt.newField();
+  var nameField = createRoomPrompt.newField('text');
   nameField.name = 'name';
-  nameField.type = 'text';
   nameField.startField = true;
-  nameField.inputCacheName = 'name';
-  nameField.promptMessage = 'Enter room name. (This is displayed at the top of the room description)\n';
+  nameField.formatPrompt('Enter room name. (This is displayed at the top of the room description)');
   createRoomPrompt.addField(nameField);
 
-  var shortDescField = createRoomPrompt.newField();
+  var shortDescField = createRoomPrompt.newField('text');
   shortDescField.name = 'short_description';
-  shortDescField.type = 'text';
-  shortDescField.inputCacheName = 'short_description';
-  shortDescField.promptMessage = 'Enter short description of room. (I have no idea where this will get displayed yet)\n';
+  shortDescField.formatPrompt('Enter short description of room. (I have no idea where this will get displayed yet)');
   createRoomPrompt.addField(shortDescField);
 
-  var fullDescField = createRoomPrompt.newField();
+  var fullDescField = createRoomPrompt.newField('multi-text');
   fullDescField.name = 'full_description';
-  fullDescField.type = 'multi';
-  fullDescField.inputCacheName = 'full_description';
-  fullDescField.promptMessage = 'Enter full room description. (This is displayed whenever a player enters the room)\n';
+  fullDescField.formatPrompt('Enter full room description. (This is displayed whenever a player enters the room)');
   createRoomPrompt.addField(fullDescField);
 
-  createRoomPrompt.setActivePrompt(createRoomPrompt);
+  createRoomPrompt.start();
 }
 
 room.prototype.editRoom = function(connection) {
