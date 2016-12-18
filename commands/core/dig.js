00ad351997630cac7fc7a19cd3fe4fabd7b33675
Allen Freeman <allen@designhammer.com>
2016-12-17 23:55:17 -0500

    Added checking for pre-existing exit to dig command

--- commands/core/dig.js
+++ commands/core/dig.js
@@ -1,17 +1,26 @@
 var Command = function() {
   this.trigger = 'quit';
   this.helpText = '';
-  this.callback = function (socket, input) {
+  this.callback = function(socket, input) {
     if (input.length === 0) {
       socket.playerSession.error('Dig where??\n');
     }
     else {
+      var roomId = socket.playerSession.character.currentRoom;
+      // It would be nonsensical to permit digging in a direction with a pre-existing exit.
+      for (i = 0; i < global.rooms.room[roomId].exits.length; ++i) {
+        exit = global.rooms.room[roomId].exits[i];
+        if (input === exit.label) {
+          socket.playerSession.error('An exit already exists in that direction');
+          break;
+        }
+      }
       fieldValues = {
         name: 'Empty space',
         short_description: 'Nothing to see here.',
         full_description: 'Empty space just waiting to be filled. Remind you of Prom night?'
       }
-      global.rooms.saveRoom(socket, fieldValues, global.rooms.createPlaceholderExit, direction);
+      global.rooms.saveRoom(socket, fieldValues, global.rooms.createPlaceholderExit, input);
     }
   }
 
