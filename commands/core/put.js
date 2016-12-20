7a8d12df128400813d9685178b123192229e336a
Allen Freeman <allen@designhammer.com>
2016-12-19 21:14:27 -0500

    Total rewrite of the put command for better structure and readability

--- commands/core/put.js
+++ commands/core/put.js
@@ -2,63 +2,110 @@ var Command = function() {
   this.trigger = 'put';
   this.helpText = '';
   this.callback = function(socket, input) {
-    var itemIndex = false;
-    var containerIndex = false;
-    var inventory = false;
-    var containerLocation = false;
-    // expected command format: put <item name> in <container name>
-    // expected command format: put <item name> <container name>
-    commandParts = input.split(' in ');
-    if (commandParts.length < 1) {
+    // Route command based on input format
+    if (input.contains(' in ')) {
+      this.hasIn(socket, input);
+    }
+    else {
+      this.noIn(socket, input);
+    }
+  }
+
+  // expected command format: put <item name> in <container name>
+  this.hasIn = function(socket, input) {
+    var commandParts = input.split(' in ');
+    // One or more arguments missing
+    if (commandParts.length < 2) {
       socket.playerSession.error('Put what where?');
       return;
     }
-    // check personal inventory for item
-    itemIndex = global.items.searchInventory(commandParts[0], 'name', socket.playerSession.character.inventory, true);
-    console.log('put item found at index:' + itemIndex);
-
-    if (itemIndex === 'false') {
-      socket.playerSession.error('You dont have a ' + commandParts[0]);
-      return;
-    }
-  // check personal inventory for container
-    containerIndex = global.items.searchInventory(commandParts[1], 'name', socket.playerSession.character.inventory, true);
-    console.log('put container found at index:' + containerIndex);
-    if (containerIndex !== false) {
-      inventory = socket.playerSession.character.inventory;
-      containerLocation = 'character inventory';
-    }
-    // TODO: add check on character EQ slots (scabbards, worn bags, etc)
     else {
-      // no? ok how about the room?
-      roomId = socket.playerSession.character.currentRoom;
-      containerIndex = global.items.searchInventory(commandParts[0], 'name', global.rooms.room[roomId].inventory, true);
+      var itemName = commandParts[0];
+      var itemIndex = this.findItemInInventory(socket, itemName);
+      // item doesn't exist
+      if (itemIndex === false) {
+        socket.playerSession.error("You don't have a " + itemName);
+        return;
+      }
 
-      if (containerIndex !== false) {
-        inventory = global.rooms.room[roomId].inventory;
-        containerLocation = 'room';
+      var containerName = commandParts[1];
+      var containerDetails = this.findContainer(socket, containerName);
+      if (containerDetails === false) {
+        socket.playerSession.error("There is no " + containerName + " here to put things in.");
+        return;
       }
+
+      if (this.isItem(socket, containerDetails) === false) {
+        socket.playerSession.error("That is not a container.");
+        return;
+      }
+
+      // Looks like we're out of potential error states, let's transfer this thing already.
+      var fieldValues = {
+        transferType: 'character-to-container',
+        item: socket.playerSession.character.inventory[itemIndex],
+        index: itemIndex,
+        containerIndex: containerDetails.index,
+        containerLocation: containerDetails.location
+      }
+      global.items.transferItemInstance(socket, fieldValues);
+
+      // It is disconcerting when commands don't provide some kind of feedback when executed, so:
+      var roomId = socket.playerSession.character.currentRoom;
+      var name = socket.playerSession.character.name;
+      // player message
+      socket.playerSession.write('You put a ' + fieldValues.item.name + ' in the ' + inventory[containerIndex].name);
+      // room message
+      global.rooms.message(socket, roomId, name + ' puts a ' + fieldValues.item.name + ' in a ' + inventory[containerIndex].name, true);
+
     }
-    if (inventory !== false) {
-      if (inventory[containerIndex].type === 'container') {
-        var fieldValues = {
-          transferType: 'character-to-container',
-          item: inventory[itemIndex],
-          index: itemIndex,
-          containerIndex: containerIndex,
-          containerLocation: containerLocation
-        }
-        global.items.transferItemInstance(socket, fieldValues);
-        var roomId = socket.playerSession.character.currentRoom;
-        var name = socket.playerSession.character.name;
-        // player message
-        socket.playerSession.write('You put a ' + fieldValues.item.name + ' in the ' + inventory[containerIndex].name);
-        // room message
-        global.rooms.message(socket, roomId, name + ' puts a ' + fieldValues.item.name + ' in a ' + inventory[containerIndex].name, true);
-      }
-      else {
-        socket.playerSession.error('That is not a container.');
-      }
+  }
+
+  // expected command format: put <item name> <container name>
+  this.noIn = function(socket, input) {
+    // TODO: come up with an approach that handles multi-word item and container names. (Ex: put black ring large sack)
+    commandParts = input.split(' ');
+  }
+
+  // check personal inventory for item
+  this.findItem = function(socket, itemName) {
+    return global.items.searchInventory(itemName, 'name', socket.playerSession.character.inventory, true);
+  }
+
+  this.findContainer = function(socket, containerName) {
+    var roomId = socket.playerSession.character.currentRoom;
+    var containerIndex = this.findItem(socket, containerName);
+    var inventoryType = 'character';
+    // If a matching container wasn't found in personal inventory check the current room.
+    if (containerIndex === false) {
+      containerIndex = global.items.searchInventory(containerName, 'name', global.rooms.room[roomId].inventory, true);
+      inventoryType = 'room';
+    }
+    if containerIndex !== false) {
+      return {index:containerIndex, location:inventoryType}
+    }
+    else {
+      return false;
+    }
+  }
+
+  this.isContainer(socket, containerDetails) {
+    switch (containerDetails.location) {
+      case 'room':
+        inventory = global.rooms.room[socket.playerSession.character.currentRoom].inventory;
+        break;
+      case 'character':
+        inventory = socket.playerSession.character.inventory;
+        break;
+      default:
+        return false;
+    }
+    // TODO: update this when item types are deprecated by flags.
+    if (inventory[containerDetails.index].type === 'container') {
+      return true;
+    }
+    else {
+      return false;
     }
   }
 }
