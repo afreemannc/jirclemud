460da5215b965b5b4afef6df7826c1ce4e667011
Allen Freeman <allen@designhammer.com>
2016-12-27 16:07:58 -0500

    Added integer field type

---
+++ prompt/fields/int.js
@@ -0,0 +1,43 @@
+function Int() {
+
+  this.name = '';
+  this.type = '';
+  this.value = false;
+  this.startField = false;
+  this.promptMessage = '';
+  this.validated = false;
+  this.conditional = false;
+
+  this.formatPrompt = function(message) {
+    this.promptMessage =  message + '\n';
+  };
+
+  this.sanitizeInput = function(input) {
+    input = input.toString().replace(/(\r\n|\n|\r)/gm,"");
+    return parseInt(input);
+  }
+
+  this.validate = function(input) {
+    if (Number.isInteger(input)) {
+      return true;
+    }
+    else {
+      this.validationError(socket, input);
+      return false;
+    }
+  }
+
+  this.validationError = function(socket, input) {
+    socket.write('"' + input + '" is not a number.\n');
+  };
+
+
+  this.cacheInput = function(input) {
+    this.value = input;
+    return true;
+  };
+}
+
+module.exports.new = function() {
+  return new Int();
+}
