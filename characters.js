3c77c45d79be53264c75319cb7e7941920e1a800
Allen Freeman <allen@designhammer.com>
2017-01-01 16:05:24 -0500

    Shuffling stuff around a bit.

--- characters.js
+++ characters.js
@@ -73,7 +73,7 @@ var Characters = function(){
       containerType: 'player_inventory',
       parentId: character.id
     }
-    global.items.loadInventory(values, session);
+    global.containers.loadInventory(values, session);
     // Raw socket write is used here since the command prompt will be displayed after "look" runs.
     session.socket.write('Welcome back ' + character.name + '\n');
     session.inputContext = 'command';
--- items/items.js
+++ items/items.js
@@ -282,48 +282,6 @@ item.prototype.transferItemInstance = function(session, fieldValues) {
   }
 }
 
-item.prototype.loadInventory = function(fieldValues, session) {
-    var inserts = [fieldValues.containerType, fieldValues.parentId];
-    var sql = `
-      SELECT
-        ii.instance_id,
-        i.name,
-        i.room_description,
-        i.full_description,
-        i.properties
-      FROM item_instance ii
-      INNER JOIN items i
-        ON i.iid = ii.iid
-      INNER JOIN container_inventory ci
-        ON ci.instance_id = ii.instance_id
-      INNER JOIN containers c
-        ON c.cid = ci.cid
-      WHERE
-        container_type = ?
-        AND parent_id = ?`;
-
-    sql = global.mysql.format(sql, inserts);
-
-    global.dbPool.query(sql, function(error, results) {
-      if (error) {
-        console.log('unable to load inventory:' + inserts);
-      }
-      else {
-        switch (fieldValues.containerType) {
-          case 'player_inventory':
-            session.character.inventory = results;
-            break;
-          case 'room':
-            global.rooms.room[fieldValues.parentId].inventory = results;
-            break;
-          default:
-            console.log('Unknown inventory type specified during load:' + inserts);
-            break;
-        }
-      }
-    });
-}
-
 /**
  * Provides a list of the content of an inventory.
  *
