98c78e5614f47a5889e4ba3ba5288032f2e6fc7f
Allen Freeman <allen@designhammer.com>
2017-01-02 11:20:30 -0500

    Upgraded move command help text

--- commands/core/move.js
+++ commands/core/move.js
@@ -1,6 +1,21 @@
 var Command = function() {
   this.trigger = 'move';
-  this.helpText = '';
+  this.helpText = `
+  Move your character from room to room. Unlike most other commands the command name
+  may be omitted when moving by just typing the direction you want to move.
+
+  %yellow%Usage:%yellow%
+         move <direction>
+         <direction>
+
+  %yellow%Example:%yellow%
+
+    > move e
+    > You travel east.
+    >
+    > w
+    > You travel west.
+  `;
   this.callback = function (session, input) {
     var roomId = session.character.currentRoom;
     var characterId = session.character.id;
