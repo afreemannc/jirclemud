7b22fab6cba506952779977f3b6e6b6f7477d438
Allen Freeman <allen@designhammer.com>
2017-01-02 01:50:47 -0500

    caching changes at the end of the night

--- commands/core/enter.js
+++ commands/core/enter.js
@@ -1,11 +1,33 @@
 var Command = function() {
-  this.trigger = 'quit';
-  this.helpText = '';
+  this.trigger = 'enter';
+  this.helpText = 'Enter a gate or portal.';
   this.callback = function (session, input) {
-    var quitMessage = global.config.quitMessage;
-    session.socket.end(quitMessage);
+    var roomId = session.character.currentRoom;
+    var name = session.character.name;
+    var itemIndex = global.containers.findItemInContainer(input, 'name', global.rooms.room[roomId].inventory, true);
+    if (itemIndex === false) {
+      session.error('There is nothing like that here for you to enter.');
+    }
+    else {
+      var portal = global.rooms.room[roomId].inventory[itemIndex];
+      if (portal.properties.flags.includes('PORTAL') === false) {
+        session.error('Enter what?');
+      }
+      else {
+        var target_rid = portal.properties.target_rid;
+        if (typeof global.rooms.room[target_rid] === undefined) {
+          // someone derped the target id so throw an innoccuous error for the user.
+          session.error('You try to enter but cannot fit into the opening.');
+          return false;
+        }
+        else {
+          session.character.currentRoom = target_rid;
+          session.socket.write('You step into the ' + portal.name + ' and disappear.');
+          global.commands.triggers.look(session, '');
+        }
+      }
+    }
   }
-
 }
 
 module.exports = new Command();
