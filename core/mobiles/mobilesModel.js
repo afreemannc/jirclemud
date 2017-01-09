f8ff3f87a1ae4e9c85a52d7327708d0ad414b0b2
Allen Freeman <allen@designhammer.com>
2017-01-09 02:44:51 -0500

    Defined table structure for mobiles.

---
+++ core/mobiles/mobilesModel.js
@@ -0,0 +1,36 @@
+module.exports.name = 'Mobile';
+module.exports.fields = {
+    mid: {
+      type: Sequelize.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true
+    },
+
+    start_rid: {
+      type: Sequelize.INTEGER.UNSIGNED
+    },
+
+    name: {
+      type: Sequelize.STRING
+    },
+
+    description: {
+      type: Sequelize.TEXT
+    },
+
+    stats: {
+      type: Sequelize.TEXT
+    },
+
+    effects: {
+      type: Sequelize.TEXT
+    },
+
+    equipment: {
+      type: Sequelize.TEXT
+    }
+
+    extra: {
+      type: Sequelize.text
+    }
+};
