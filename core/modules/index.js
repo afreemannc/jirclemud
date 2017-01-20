6472db06a4873735bcc10b9cdda428f26463a5e7
Allen Freeman <allen@designhammer.com>
2017-01-20 01:56:40 -0500

    Admin screens for enabling and disabling modules works.

--- core/modules/index.js
+++ core/modules/index.js
@@ -26,7 +26,6 @@ var modules = function() {
    *   Returns an object containing modules keyed by name.
    */
   this.findAllModules = function(dir) {
-   console.log('dir:' + dir);
    fs.readdirSync(dir)
       .filter(function(file) {
         var filepath = path.join(dir, file);
@@ -46,11 +45,16 @@ var modules = function() {
 
   this.addModuleStatus = function() {
     var Module = Models.Module;
-    Module.findAll({attributes: ['name', 'status'], where:{status: 1}}).then(function(instances) {
+    Module.findAll({attributes: ['mid', 'name', 'status']}).then(function(instances) {
       instances.forEach(function(instance) {
         var name = instance.get('name');
         var status = instance.get('status');
+        var mid = instance.get('mid');
+        // If this isn't explicitly converted to a string user input won't match when compared via ===
+        // This breaks module selection fields that display mid as the select option.
+        mid = mid.toString();
         Modules.availableModules[name].status = status;
+        Modules.availableModules[name].mid = mid;
       });
     });
   }
@@ -60,6 +64,9 @@ var modules = function() {
  * Load all modules that have been enabled.
  */
 modules.prototype.loadEnabledModules = function() {
+  Modules.findAllModules(Modules.moduleDir);
+  Modules.addModuleStatus();
+
   var Module = Models.Module;
 
   Module.findAll({where:{status: 1}}).then(function(instances) {
@@ -83,8 +90,6 @@ modules.prototype.loadEnabledModules = function() {
 }
 
 modules.prototype.moduleAdmin = function(session) {
-  Modules.findAllModules(Modules.moduleDir);
-  Modules.addModuleStatus();
   moduleAdminPrompt = Prompt.new(session, Modules.saveModuleChanges);
 
   adminStartField = moduleAdminPrompt.newField('select');
@@ -95,7 +100,7 @@ modules.prototype.moduleAdmin = function(session) {
 
   adminEnableField = moduleAdminPrompt.newField('select');
   adminEnableField.name = 'enable';
-  adminEnableField.options = Modules.listModuleOptions('disabled');
+  adminEnableField.options = Modules.listModuleOptions(0);
   adminEnableField.formatPrompt('Select a module to enable:');
   adminEnableField.conditional = {
     field: 'start',
@@ -105,7 +110,7 @@ modules.prototype.moduleAdmin = function(session) {
 
   adminDisableField = moduleAdminPrompt.newField('select');
   adminDisableField.name = 'disable';
-  adminDisableField.options = Modules.listModuleOptions('enabled');
+  adminDisableField.options = Modules.listModuleOptions(1);
   adminDisableField.formatPrompt('Select a module to disable:');
   adminDisableField.conditional = {
     field: 'start',
@@ -113,65 +118,81 @@ modules.prototype.moduleAdmin = function(session) {
   }
   moduleAdminPrompt.addField(adminDisableField);
 
-  // List option is handled by a separate screen.
-  console.log('module admin invoked');
   moduleAdminPrompt.start();
 }
 
 modules.prototype.saveModuleChanges = function(session, fieldValues) {
-  var Module = Models.Module;
-  console.log('field values:');
-  console.log(fieldValues);
-  // enable
   if (fieldValues.start === 'enable') {
-    var allModules = this.listAllModules();
-    var selectedModule = allModules[fieldValues.enable];
-    var values = {
-      name: selectedModule.name,
-      description: selectedModule.description,
-      status: 1,
-      filepath: selectedModule.filepath,
-      version: selectedModule.version,
-      features: selectedModule.features
-    }
-    Module.findOrCreate(values).then(function(instance, created) {
-      if (created === false) {
-        Module.update({status:1}, {where:{mid:fieldValues.enable}});
+    var moduleStatus = 1;
+    var name = fieldValues.enable;
+  }
+  else {
+    var moduleStatus = 0;
+    var name = fieldValues.disable;
+  }
+
+  switch (fieldValues.start) {
+    case 'enable':
+      var module = Modules.availableModules[name];
+      if (typeof module.mid === 'undefined') {
+        var Module = Models.Module;
+        var values = {
+          name: module.name,
+          description: module.description,
+          status: 1,
+          filepath: module.filePath,
+          varsion: module.version
+        }
+        Module.create(values).then(function() {
+          session.write('Module status updated. This change will not take effect until the mud is restarted.');
+        });
+        break;
       }
-    });
-    return true;
-  }
+      // intentionally fall through to update if there is already a record
+      // for this module in the db (true if mid is set).
+    case 'disable':
+      var Module = Models.Module;
 
-  // disable
-  if (fieldValues.start === 'disable') {
-    Module.update({status:0}, {where:{mid:fieldValues.disable}}).then(function() {
-      session.write('Module disabled.');
-    });
-    return true;
-  }
-
-  // list
-  if (fieldValues.start === 'list') {
-    // display module list.
-    var output = '';
-    var moduleKeys = Object.keys(Modules.availableModules);
-    for (i = 0; i < moduleKeys.length; ++i) {
-      var module = Modules.availableModules[moduleKeys[i]];
-      output += module.name + ' : ' + module.status + ' : ' + module.description + '\n';
-    }
-    session.write(output);
-    return true;
-  }
-
-  // quit
-  if (fieldValues.start === 'quit') {
-    // back out to arch main screen.
-    return true;
+      Module.update({status:moduleStatus}, {where:{name:name}}).then(function() {
+        session.write('Module status updated. This change will not take effect until the mud is restarted.');
+        Modules.availableModules[name].status = moduleStatus;
+      });
+      session.inputContext = 'command';
+      Modules.moduleAdmin(session);
+      break;
+    case 'list':
+      var output = '';
+      var moduleKeys = Object.keys(Modules.availableModules);
+      for (i = 0; i < moduleKeys.length; ++i) {
+        var module = Modules.availableModules[moduleKeys[i]];
+        output += module.name + ' : ' + module.status + ' : ' + module.description + '\n';
+      }
+      session.write(output);
+      session.inputContext = 'command';
+      Modules.moduleAdmin(session);
+      return true;
+      break;
+    case 'quit':
+      session.inputContext = 'command';
+      Admin.listTasks(session);
+      return true;
+      break;
+    default:
+      session.inputContext = 'command';
+      return false;
   }
 }
 
 modules.prototype.listModuleOptions = function(status) {
-  return {h:'hi'};
+  var moduleKeys = Object.keys(Modules.availableModules);
+  var options = {};
+  for (var i = 0; i < moduleKeys.length; ++i) {
+    var module = Modules.availableModules[moduleKeys[i]];
+    if (module.status === status) {
+      options[i] = module.name;
+    }
+  }
+  return options;
 }
 
 module.exports = new modules();
