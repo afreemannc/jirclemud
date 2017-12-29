e528df99a492fdc378a3ac6f578a6b3e75f5e61a
Allen Freeman <allen@designhammer.com>
2017-12-29 11:14:47 -0500

    Roughing in combat API

--- core/combat/index.js
+++ core/combat/index.js
@@ -5,26 +5,53 @@
 
 module.exports.new = function () {
   // list of participants
+  this.id = 'combat' + new Date().getTime();
   this.participants = {
     mobs:[],
     players:[]
   }
   // start event
   this.start = function() {
+    // create event queue
+    var tic = Tics.addQueue('combat' + this.id, combatTicInterval);
 
+    tic.event.on('combat' + this.id, function() {
+      this.combatRound();
+    });
+    Tics.startQueue('combat' + this.id);
   }
   // method for adding participants
   this.addParticipant = function(type, participant) {
     if (typeof this.participants[type] !== 'undefined') {
       this.participants[type].push(participant);
+      // TODO: emit combat join event for participant.
     }
   }
   // mentod for removing participants (flee, teleport, summoned away, dead, etc)
-
-  // method to iterate participant list and fire base attack event
-  this.combatRound = function() {
-
+  this.removeParticipant = function(type, participant) {
+    if (typeof this.participants[type] !== 'undefined') {
+      var index = this.participants[type].indexOf(participant);
+      this.participants[type].splice(index, 1);
+      // TODO: emit combat exit event for participant.
+    }
   }
+
+  this.combatRound = function() {
+    // TODO: (somewhere)
+    // - process input queue (if needed)
+    // -- if input is lagged by a command this turn skip
+    // -- process queue one input at a time until end is reached or a combat command is encountered.
+    //
+    // - handle base attacks
+    // - handle any executed commands/skills as needed
+  }
+
+  // method to end combat
+  this.end = function() {
+    // TODO: halt combat queue.
+    // TODO: emit combat end event for participants
+  }
+
   // update input parser for the following:
     // method for creating input queue and setting queue flag, turn skip count
     // method for parsing contents of input queue
@@ -34,12 +61,3 @@ module.exports.new = function () {
    // add support for turn skip/lag count on combat commands
   // end event
 });
-
-
-modile.exports.end = function() {
-  // unset combat flag on all participants
-
-  // parse contents of input queues and unset queue flag
-  // end event
-  // delete combat object
-}
