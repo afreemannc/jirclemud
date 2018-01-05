113f236ef2f250cffa73e8115653bbf68fa77fe7
Allen Freeman <allen@designhammer.com>
2018-01-05 00:24:00 -0500

    Speculative startEdit() method, intended to eliminate the need to manually enumerate every possible combination of edit prompts required to complement mob/item/zone/character creation.

--- core/prompt/index.js
+++ core/prompt/index.js
@@ -44,6 +44,33 @@ function promptSystem() {
     console.log(newPrompt);
     newPrompt.start();
   }
+
+  this.startEdit(id, session, fieldName, values) {
+    var cachedPrompt = this.registered[id];
+    var newPrompt = this.Prompt.new(id, sessions, cachedPrompt.completionCallback, cachedPrompt.quittable);
+    var fieldNames = Object.keys(cachedPrompt.fields);
+    for (var i = 0; i < fieldNames.length; ++i) {
+      // We really don't want to permanently alter the parent prompt here.
+      var currentField = {...cachedPrompt.fields[fieldNames[i]]};
+      // Any fields other than the edit field should be converted to value fields.
+      if (fieldName !== fieldNames[i]) {
+        currentField.type = 'value';
+        currentField.value = values[fieldNames[i]];
+        currentField.conditional = {};
+      }
+      var newField = newPrompt.newField(currentField.type);
+      Object.assign(newField, currentField);
+      var replaceInPrefix = false;
+      if (typeof currentField.replaceInPrefix !== 'undefined') {
+        replaceInPrefix = currentField.replaceInPrefix;
+      }
+      if (newField.formatPrompt !== false) {
+        console.log('format prompt type:' + typeof newField.formatPrompt);
+        newField.formatPrompt(currentField.title + '\n%green%Currently: ' + values[fieldName] + '%green%', replaceInPrefix);
+      }
+      newPrompt.addField(newField);
+    }
+  }
 }
 
 module.exports = new promptSystem();
