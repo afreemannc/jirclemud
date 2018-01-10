598c2b8acf1545a5052aba5a349f7a9cd47a07d4
Allen Freeman <allen@designhammer.com>
2018-01-10 03:14:42 -0500

    PoC of room output altering

--- modules/builder/index.js
+++ modules/builder/index.js
@@ -3,6 +3,19 @@ function Module() {
   this.description = 'In-game world building commands.'
   this.install = false;
   this.features = ['commands', 'prompts'];
+  this.load = function() {
+    // Implement BUILDER perm
+
+    // Overrride room display methods so output can be altered in response to BUILDER perm
+    Rooms.displayRoomName = function(room, session) {
+      if (!Characters.hasPerm(session, 'BUILDER') && !Character.hasPerm(session, 'ADMIN')) {
+        return "\n%bold%%room.name%%bold%\n";
+      }
+      else {
+        return "\n%bold%%room.name%%bold% %green%[rid:%room.rid%]%green%\n";
+      }
+    }
+  }
 }
 
 module.exports = new Module();
